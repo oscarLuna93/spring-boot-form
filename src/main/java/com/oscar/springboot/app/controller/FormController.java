da0eedd55ac66c475268068096a532a5421a2502
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-17 16:12:05 -0600

    manejo de errores en controlador

--- src/main/java/com/oscar/springboot/app/controller/FormController.java
+++ src/main/java/com/oscar/springboot/app/controller/FormController.java
@@ -1,7 +1,13 @@
 package com.oscar.springboot.app.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -18,8 +24,21 @@ public class FormController {
 	}
 	
 	@PostMapping("/form")
-	public String procesarFormulario(Usuario usuario, Model model) {
+	public String procesarFormulario(@Valid Usuario usuario, BindingResult result, Model model) {
+
 		model.addAttribute("titulo", "Resultado form");
+
+		if (result.hasErrors()) {
+			Map<String, String> errores = new HashMap<String, String>();
+			result.getFieldErrors().forEach(error -> {
+				errores.put(error.getField(), "El campo: ".concat(error.getField()).concat(" ").concat(error.getDefaultMessage()));
+			});
+			
+			model.addAttribute("error", errores);
+			
+			return "form";
+		}
+
 		model.addAttribute("usuario", usuario);
 
 		return "resultado";

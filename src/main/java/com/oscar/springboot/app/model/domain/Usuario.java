5d31025a985ba3ee89bb36afeeb48a300d5a2bb3
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-17 17:30:45 -0600

    se agrega campos a formulario

--- src/main/java/com/oscar/springboot/app/model/domain/Usuario.java
+++ src/main/java/com/oscar/springboot/app/model/domain/Usuario.java
@@ -4,6 +4,14 @@ import javax.validation.constraints.NotEmpty;
 
 public class Usuario {
 	
+	private String identificador;
+	
+	@NotEmpty
+	private String nombre;
+	
+	@NotEmpty
+	private String apellido;
+	
 	@NotEmpty
 	private String username;
 	
@@ -13,6 +21,18 @@ public class Usuario {
 	@NotEmpty
 	private String email;
 	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 	public String getUsername() {
 		return username;
 	}
@@ -30,6 +50,12 @@ public class Usuario {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public String getIdentificador() {
+		return identificador;
+	}
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
 	}
 
 }

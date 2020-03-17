a05ff93d36fb68824efd87188cf5fa90b03f472e
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-17 17:41:18 -0600

    persistencia de datos en sesion

--- src/main/java/com/oscar/springboot/app/controller/FormController.java
+++ src/main/java/com/oscar/springboot/app/controller/FormController.java
@@ -7,22 +7,28 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.oscar.springboot.app.model.domain.Usuario;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 	
 	@GetMapping("/form")
 	public String form(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setIdentificador("id-1143");
+		
 		model.addAttribute("titulo", "Crear Usuario");
-		model.addAttribute("usuario", new Usuario());
+		model.addAttribute("usuario", usuario);
 		
 		return "form";
 	}
 	
 	@PostMapping("/form")
-	public String procesarFormulario(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesarFormulario(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
 
 		model.addAttribute("titulo", "Resultado form");
 
@@ -31,7 +37,7 @@ public class FormController {
 		}
 
 		model.addAttribute("usuario", usuario);
-
+		status.setComplete();
 		return "resultado";
 	}
 }

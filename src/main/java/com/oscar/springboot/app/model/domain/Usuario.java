a70e44a2e24a183aa02bceac603d954e7bde6fa4
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-17 16:11:52 -0600

    reglas de validacion

--- src/main/java/com/oscar/springboot/app/model/domain/Usuario.java
+++ src/main/java/com/oscar/springboot/app/model/domain/Usuario.java
@@ -1,9 +1,16 @@
 package com.oscar.springboot.app.model.domain;
 
+import javax.validation.constraints.NotEmpty;
+
 public class Usuario {
 	
+	@NotEmpty
 	private String username;
+	
+	@NotEmpty
 	private String password;
+	
+	@NotEmpty
 	private String email;
 	
 	public String getUsername() {

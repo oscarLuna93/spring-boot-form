24c5fda925e2843b5b93287111f65406270c6b57
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-17 15:14:24 -0600

    procesando modelo como parametro

--- src/main/java/com/oscar/springboot/app/controller/FormController.java
+++ src/main/java/com/oscar/springboot/app/controller/FormController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.oscar.springboot.app.model.domain.Usuario;
 
@@ -19,17 +18,7 @@ public class FormController {
 	}
 	
 	@PostMapping("/form")
-	public String procesarFormulario(Model model,
-			@RequestParam String username,
-			@RequestParam String password,
-			@RequestParam String email) {
-		
-		Usuario usuario = new Usuario();
-		
-		usuario.setUsername(username);
-		usuario.setPassword(password);
-		usuario.setEmail(email);
-		
+	public String procesarFormulario(Usuario usuario, Model model) {
 		model.addAttribute("titulo", "Resultado form");
 		model.addAttribute("usuario", usuario);
 

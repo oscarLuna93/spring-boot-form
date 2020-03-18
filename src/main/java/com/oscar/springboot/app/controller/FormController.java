590635dba30487009f4ee2abcf5ae0a9c72376a1
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-18 10:15:46 -0600

    validacion de usuario con  Validator

--- src/main/java/com/oscar/springboot/app/controller/FormController.java
+++ src/main/java/com/oscar/springboot/app/controller/FormController.java
@@ -2,6 +2,7 @@ package com.oscar.springboot.app.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,11 +12,15 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import com.oscar.springboot.app.model.domain.Usuario;
+import com.oscar.springboot.app.validation.UsuarioValidador;
 
 @Controller
 @SessionAttributes("usuario")
 public class FormController {
 	
+	@Autowired
+	private UsuarioValidador validador;
+
 	@GetMapping("/form")
 	public String form(Model model) {
 		Usuario usuario = new Usuario();
@@ -29,7 +34,7 @@ public class FormController {
 	
 	@PostMapping("/form")
 	public String procesarFormulario(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
-
+		validador.validate(usuario, result);
 		model.addAttribute("titulo", "Resultado form");
 
 		if (result.hasErrors()) {

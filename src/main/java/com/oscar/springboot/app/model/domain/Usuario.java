fdb852db1924c7420cd2f5dcfaa5220b73bd27fa
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-24 18:14:37 -0600

    validacion de fecha con past y cambio de formato

--- src/main/java/com/oscar/springboot/app/model/domain/Usuario.java
+++ src/main/java/com/oscar/springboot/app/model/domain/Usuario.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -40,7 +41,8 @@ public class Usuario {
 	private Integer cuenta;
 	
 	@NotNull
-	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	@Past
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date fechaNacimiento;
 	
 	public String getNombre() {
